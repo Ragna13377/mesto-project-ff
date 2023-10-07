f919d9ea681a02f22fda78be3a817b1749ad92a8
Ivan Alexandrov <mordrag.gothic@yandex.ru>
2023-10-08 01:34:52 +0300

    After formatter and Linter

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 const cardTemplate = document.querySelector('#card-template').content;
 const cardContainer = document.querySelector('.places__list');
+const cardDeleteFunction = (e) => e.closest('.card').remove();
 
 function showPopup(...popup) {
   for (let item of popup) {
@@ -18,7 +19,10 @@ function showPopup(...popup) {
         if (popupForm.name === 'new-place') {
           const placeName = popupForm.querySelector("[name='place-name']"),
             link = popupForm.querySelector("[name='link']");
-          showCard({ name: placeName.value, link: link.value });
+          showCard(cardDeleteFunction, {
+            name: placeName.value,
+            link: link.value,
+          });
           placeName.value = link.value = '';
         } else {
           const name = popupForm.querySelector("[name='name']"),
@@ -40,13 +44,12 @@ function showPopup(...popup) {
     }
   }
 }
-function showCard(...cards) {
+function showCard(deleteFunc, ...cards) {
   for (let item of cards) {
     try {
       if (Array.isArray(item) || typeof item !== 'object')
         throw new Error('В функцию не передан объект');
       const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
-      const cardPopup = document.querySelector('.popup_type_image');
       cardItem.querySelector('.card__image').src = item.link;
       cardItem.querySelector('.card__title').textContent = item.name;
       cardItem
@@ -56,15 +59,21 @@ function showCard(...cards) {
         );
       cardItem
         .querySelector('.card__delete-button')
-        .addEventListener('click', (e) =>
-          e.currentTarget.closest('.card').remove()
-        );
+        .addEventListener('click', (e) => cardDeleteFunction(e.currentTarget));
+      const cardPopup = document.querySelector('.popup_type_image');
       cardItem.querySelector('.card__image').addEventListener('click', () => {
-        document
-          .querySelector('.popup_type_image')
-          .classList.add('popup_is-opened');
+        cardPopup.classList.add('popup_is-opened');
         cardPopup.querySelector('.popup__image').src = item.link;
         cardPopup.querySelector('.popup__caption').textContent = item.name;
+        const closePopup = () => {
+          cardPopup.classList.remove('popup_is-opened');
+          cardPopup
+            .querySelector('.popup__close')
+            .removeEventListener('click', closePopup);
+        };
+        cardPopup
+          .querySelector('.popup__close')
+          .addEventListener('click', closePopup);
       });
       cardContainer.append(cardItem);
     } catch (error) {
@@ -77,4 +86,4 @@ showPopup(
   { trigger: 'profile__add-button', popupBody: 'popup_type_new-card' },
   { trigger: 'profile__edit-button', popupBody: 'popup_type_edit' }
 );
-showCard(...initialCards);
+showCard(cardDeleteFunction, ...initialCards);
