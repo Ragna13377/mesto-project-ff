b7a0d2e54d0db79baab1604afa0cf68fc5214b4b
Ivan Alexandrov <mordrag.gothic@yandex.ru>
2023-11-10 01:07:45 +0300

    after 1 review

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,45 +1,11 @@
-export {initialCards, createCard, deleteCard, likeCard}
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
-  },
-];
-
-function createCard(card, container) {
-  if(card !== null) container.prepend(addCard(card));
-}
-
 const deleteCard = (e) => e.target.closest('.card').remove();
 const likeCard = (e) => e.target.classList.toggle('card__like-button_is-active')
 
-function addCard(card) {
+function makeCard(card, template) {
   try {
     if (Array.isArray(card) || typeof card !== 'object')
       throw new Error('Не передан объект данных');
-    const cardTemplate = document.querySelector('#card-template').content;
-    const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardItem = template.querySelector('.card').cloneNode(true);
     cardItem.querySelector('.card__image').src = card.link;
     cardItem.querySelector('.card__title').textContent = card.name;
     return cardItem;
@@ -49,6 +15,6 @@ function addCard(card) {
   }
 }
 
-
+export { deleteCard, likeCard, makeCard }
 
 
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,27 +1,81 @@
 import '../pages/index.css';
-import {createCard, initialCards} from "@/components/cards";
-import {openPopup, initPopup, submitEditProfilePopup, submitAddCardPopup, setDefaultPopup, setEditInfoPopup, setImageInfoPopup} from "@/components/modal";
+import { initialCards } from "./cards.js"
+import { deleteCard, likeCard, makeCard } from "@/components/cards.js"
+import { openPopup, closePopup } from "@/components/modal.js"
 
-export const cardContainer = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+const cardContainer = document.querySelector('.places__list');
+const popupNewCard = document.querySelector('.popup_type_new-card');
+const triggerNewCard = document.querySelector('.profile__add-button');
+const popupTypeEdit = document.querySelector('.popup_type_edit');
+const popupTypeImage = document.querySelector('.popup_type_image');
+const triggerTypeEdit = document.querySelector('.profile__edit-button');
+const title = document.querySelector('.profile__title');
+const description = document.querySelector('.profile__description');
+const popupTitle = popupTypeEdit.querySelector('input[name="name"]');
+const popupDescription = popupTypeEdit.querySelector('input[name="description"]');
 
-for (let item of initialCards) {
-  createCard(item, cardContainer);
+const setProfileInfo = () => {
+  if(popupTitle.value === '' && popupDescription.value === '') {
+    popupTitle.value = title.textContent;
+    popupDescription.value = description.textContent;
+  }
 }
 
-openPopup(cardContainer, document.querySelector('.popup_type_image'), setImageInfoPopup)
+const setImageInfoPopup = (e) => {
+  popupTypeImage.querySelector('.popup__image').src = e.target.src;
+  popupTypeImage.querySelector('.popup__caption').textContent = e.target.closest('.card').querySelector('.card__description').textContent;
+}
 
-initPopup({
-  container: 'popup_type_edit',
-  trigger: 'profile__edit-button',
-  submitAction: submitEditProfilePopup,
-  showAction: setEditInfoPopup,
-});
-initPopup({
-  container: 'popup_type_new-card',
-  trigger: 'profile__add-button',
-  submitAction: submitAddCardPopup,
-  showAction: setDefaultPopup,
-});
+const submitAddCardPopup = (formData) => {
+  addCard({ name: formData.get('place-name'), link: formData.get('link') });
+}
 
+
+const submitEditProfilePopup = (formData) => {
+  title.textContent = formData.get('name');
+  description.textContent = formData.get('description');
+};
+
+const closePopupHandler = (e, popup) => {
+  if(e.key === 'Escape' ||
+    e.target.classList.contains('popup__close') ||
+    e.target.closest('.popup__content') === null) {
+    if(e.target.classList.contains('popup__close')) {
+      popup.querySelector('.popup__form')?.reset();
+    }
+    closePopup(popup);
+  }
+}
+
+const openPopupHandler = (popup, submitAction = null, setAction = null, e = null) => {
+  openPopup(popup);
+  if(setAction) setAction(e);
+  popup.querySelector('.popup__form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    if(submitAction) { submitAction(formData);}
+    e.currentTarget?.reset();
+    closePopup(popup);
+  });
+  popup.addEventListener('click', (e) => closePopupHandler(e, popup));
+  document.addEventListener('keydown', (e) => closePopupHandler(e, popup));
+};
+
+function addCard(card) {
+  if(card !== null) {
+    const createdCard = makeCard(card, cardTemplate)
+    createdCard.querySelector('.card__delete-button').addEventListener('click', deleteCard)
+    createdCard.querySelector('.card__like-button').addEventListener('click', likeCard)
+    createdCard.querySelector('.card__image').addEventListener('click', (e) => openPopupHandler(popupTypeImage, null, setImageInfoPopup, e))
+    cardContainer.prepend(createdCard);
+  }
+}
+
+for (let item of initialCards) {
+  addCard(item);
+}
+triggerNewCard.addEventListener('click', () => openPopupHandler(popupNewCard, submitAddCardPopup));
+triggerTypeEdit.addEventListener('click', () => openPopupHandler(popupTypeEdit, submitEditProfilePopup, setProfileInfo));
 
 
